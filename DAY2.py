d45844ee3d2cd6dca6a088950cf9ec95fc378ff2
dev-satyamjha <realsatyam02@gmail.com>
2025-10-08 23:29:40 +0530

    Upload Day 2 final report and 2nd project

--- DAY2.py
+++ DAY2.py
@@ -30,3 +30,42 @@ a: float = 0.1
 b: float = 0.2
 sum: float = round(a + b, 1)
 print(sum == 0.3)
+
+# Strings
+
+text: str = "Hello, I am Satyam"
+text2: str = "\"Hello! Feels good to be back\""  # Escaping double quotes
+text3: str = 'I\'m you\'re best friend'  # Escaping single quotes
+
+# String Concatenation
+name: str = "Satyam"
+action: str = " is salying"
+sentence: str = name + action
+print(sentence)
+
+# multi line strings
+
+quote: str = '''
+ I am not a good guy,
+ I am not a bad guy,
+ I am the guy!
+ '''
+print(quote)
+
+#booleans
+
+is_awake: bool = True
+is_snake: bool = False
+
+print(is_snake)
+
+print (True+False) # Bools addition
+
+print (int(True)) # Display Bool's value
+
+#type conversions
+
+print(10+20) #normal addition
+print(str(10) + '20') #string addition
+print (float(10.5)) #float casting
+print (int(10.9)) #int casting
