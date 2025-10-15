fc934d31ac1dff6678127bbbf49268e99de40a0c
dev-satyamjha <realsatyam02@gmail.com>
2025-10-15 23:54:22 +0530

    Day 4 progress

--- DAY4.py
+++ DAY4.py
@@ -46,3 +46,29 @@ d1: dict [str, str] = {'HEY', 'HELLO', 'HI'}
 d2: dict [str, str] = {'HELLO', 'HOW', 'ARE' , 'YOU'}
 d1 |= d2
 print(d1)
+
+# Comparison Operators
+number: int = 32
+print(number==10)
+num: int = 32
+print(num!= 30)
+print(num>= 32)
+print(num<=43)
+print(number>21)
+print(number<21)
+
+num1: int = 12
+print(10< num1 < 24)
+print('-' * 20)
+
+# Logical Operators
+name: str = 'Harsh'
+age: float = 19.5
+print(name == 'Harsh' and age < 22)
+a: bool = False
+b: bool = True
+c: bool = True
+print(a or b or c)
+print(a and b and c)
+print(not a == False)
+print(not(a and b))
