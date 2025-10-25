00b93687d225a0cacd57160de931522da5d74a12
dev-satyamjha <realsatyam02@gmail.com>
2025-10-25 16:29:43 +0530

    Expense splitter complete with modifications

--- Projects/Expense_Splitter.py
+++ Projects/Expense_Splitter.py
@@ -1,19 +1,29 @@
 #Step 1
-from typing_extensions import List
-
-
 print("---WELCOME TO EXPENSE SPLITTER---")
-total_bill: float = float(input("1. Enter the total amount in bill to split: "))
+while True:
+    total_input = input("1. Enter the total amount in bill to split: ").strip()
+
+    if total_input == "":
+        print("You didn’t enter any amount... Try again.")
+        continue
+    if total_input.isdigit():
+        total_bill = float(total_input)
+        break
+    else:
+        print("Please enter a valid number...")
 
 #Step 2
 people: list[str] = []
-print("2. Add Participants (Press Enter on an empty line when you are done): ")
+print("2. Add Participants (Type 'Done' when names are entered): ")
 while True:
     input_name: str = input("Names: ").capitalize()
     if input_name.strip() == '':
-        break
+        print("No participant is added..Please add one!!")
+        continue
     elif input_name in people:
         print("Name is already present, please try with a different name...!!")
+    elif input_name.capitalize() == 'Done':
+        break
     else:
         people.append(input_name)
 
@@ -23,21 +33,41 @@ print('(Type "even" if you want to split the bill equally.)')
 people_dict: dict[str, float] = {}
 total_percent: float = 100.0
 for person in people:
-    percent_input: str = input(f"[{total_percent:.0f}% remaining] {person.capitalize()}: ").capitalize()
-
-    if percent_input.strip() == '':
-        percent_input = '0'
-    elif percent_input == 'Even':
-        for nested_person in people:
-            people_dict[nested_person] = (1/len(people)) * total_bill
+    while True:
+        percent_input: str = input(f"[{total_percent:.0f}% remaining] {person.capitalize()}: ").capitalize()
+        if percent_input.strip() == '':
+            print("You didn't enter any value..Try again(Enter 0 if you don't want to split): ")
+            continue
+        elif percent_input == 'Even':
+            for nested_person in people:
+                people_dict[nested_person] = (1/len(people)) * total_bill
+            total_percent = '0'
+            break
+        if not percent_input.isdigit():
+            print("Enter a valid percentage >= 0")
+            continue
+        percent_value = float(percent_input)
+        if percent_value > 100:
+            print("Enter a valid percentage <= 100...")
+            continue
+        if percent_value > total_percent:
+            print(f"You have exceeded the total split share in bill...You only have {total_percent:.2f}% left!!")
+            continue
+        people_dict[person] = (percent_value / 100) * total_bill
+        total_percent -= percent_value
         break
 
-    else:
-        people_dict[person] = (float(percent_input)/100 * total_bill)
-        total_percent -= float(percent_input)
+    if percent_input == "Even":
+        break
+
+total_split_amount = sum(people_dict.values())
+amount_left = total_bill - total_split_amount
 
 # Step 4
 print('\n---Split Summary---')
 for name, share in people_dict.items():
     print(f'{name.capitalize():10}: ₹{share:,.2f}')
 print('----------------------')
+print(f'Total amount splitted:  {total_split_amount}')
+print(f'Amount still left:      {amount_left}')
+print('------------------------------')
