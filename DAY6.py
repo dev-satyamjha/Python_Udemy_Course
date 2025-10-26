cdea84982f601bf2388c09ca57ba0ea1f60a280a
dev-satyamjha <realsatyam02@gmail.com>
2025-10-26 23:26:59 +0530

    Day 6 Beginnings..

---
+++ DAY6.py
@@ -0,0 +1,22 @@
+#Functions
+def loop():
+    for i in range(5):
+        print(f"Looping till {i+1}")
+loop()
+loop()
+print("----------")
+
+def greet():
+    print("Hey, Jai Shree Ram!!")
+for i in range(3):
+    greet()
+
+#Pass
+def jump():
+    pass
+
+test: bool = False
+if test:
+    pass
+else:
+    print("Test is not happening tommorow!!")
