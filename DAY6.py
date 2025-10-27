9982cf1d35214837cb3a92b6750c3639a73520a5
dev-satyamjha <realsatyam02@gmail.com>
2025-10-27 23:37:40 +0530

    Day 6 progress..

--- DAY6.py
+++ DAY6.py
@@ -20,3 +20,23 @@ if test:
     pass
 else:
     print("Test is not happening tommorow!!")
+
+#Parameters
+def greet(name: str, language: str):
+    if language == 'IN':
+        print(f"Namaste, {name}!!")
+    elif language == 'EN':
+        print(f"Cheers, {name}!!")
+    else :
+        print(f"Hello, {name}!!")
+
+greet('Satyam', 'IN')
+greet('Roman', 'EN')
+greet('Patron', 'IT')
+
+def loop(times:int, text: str):
+    for i in range(times):
+        print(f"{text} : {i+1}")
+loop(1, 'A')
+loop(2, 'B')
+loop(3, 'C')
