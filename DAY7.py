39d5cffc390c6441db33cc186c4e82f391f01f95
dev-satyamjha <realsatyam02@gmail.com>
2025-10-31 23:55:32 +0530

    Day 7 progress..

--- DAY7.py
+++ DAY7.py
@@ -24,3 +24,32 @@ def setup1(Version : str , **kwargs):
     if 'volume' in kwargs.keys():
         print(f"Volume set to: {kwargs.get('volume')}")
 setup1(Version='2.0', **settings )
+
+#Positional , Keywords Arguments(* & /)
+
+def add(first:int, second: int , /) -> None:
+    print(first+second)
+
+add(1, 2)
+
+def greet1(names: list[str], * , greeting: str ):
+    for name in names:
+        print(f"{greeting} {name}")
+greet1(['Satyam', 'James', 'Meowth'], greeting= 'Hello...')
+
+def calc(a:float, b: float, /, *, operation: str) -> None:
+    if operation == 'add':
+        print(a + b)
+    elif operation == 'subtract':
+        print(a - b)
+    elif operation == 'multiply':
+        print(a * b)
+    elif operation == 'divide':
+        print(a/b)
+    else:
+        print('Can\'t Calculate it buddy..!!')
+calc(9,6, operation='multiply')
+
+def calc1(a: int, /, b:int, *, c:int) -> None:
+    print(sum([a,b,c]))
+calc1(23, b=34, c=56)
