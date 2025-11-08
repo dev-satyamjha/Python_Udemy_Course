8743a2c5230a485d4bbb6cdebe06b6dc66099fef
dev-satyamjha <satyamjha.me@gmail.com>
2025-11-08 23:55:30 +0530

    Day 10 starts...

---
+++ DAY10.py
@@ -0,0 +1,14 @@
+numbers1 : list[int] = []
+numbers2 : list[int] = [1]
+
+if numbers1:
+    for number in numbers1:
+        print(number)
+else:
+    print("No Numbers:....")
+
+if numbers2:
+    for number in numbers2:
+        print(number)
+else:
+    print("No Numbers:....")
