1fe626e35bcbe61ab87c56db8988f374218d3ede
dev-satyamjha <satyamjha.me@gmail.com>
2025-11-09 23:59:46 +0530

    Day 10 progress...

--- DAY10.py
+++ DAY10.py
@@ -1,3 +1,7 @@
+from decimal import Decimal
+
+# Truthy
+
 numbers1 : list[int] = []
 numbers2 : list[int] = [1]
 
@@ -12,3 +16,31 @@ if numbers2:
         print(number)
 else:
     print("No Numbers:....")
+
+pi = 3.14
+name = 'Silen'
+elements = [0,1,2,3,4]
+users={'Ben' : 0}
+unique = {None, 0}
+
+#Falsy
+
+zero_float = 0
+empty_sr = ''
+empty_dict = {}
+emp_list = []
+emp_set = set()
+none_type = None
+
+print(bool(pi))
+print(bool(zero_float))
+print(bool(unique))
+print(bool(none_type))
+
+#Float problem
+
+a = Decimal(0.1)
+b = Decimal(0.2)
+c = Decimal(0.3)
+
+print(a)
