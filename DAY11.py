d7b02e2f17aaed92b67217fae06e7b1f2d7734eb
dev-satyamjha <satyamjha.me@gmail.com>
2025-11-13 22:07:56 +0530

    Day 11 progress

--- DAY11.py
+++ DAY11.py
@@ -8,3 +8,19 @@ def main() -> None:
     greet(people)
 if __name__ == '__main__':
     main()
+
+# List Comprehensions
+
+numbers: list[int] = [1, 2, 3, 4, 5, 6, 7, 12, 10, 14]
+squared: list[int] = [n**2 for n in numbers]
+print(squared)
+
+str_numbers: list[str] = [str(n) for n in numbers]
+print(str_numbers)
+
+text: str = "Hello Owls, Welcome to my nest!!"
+vowels: list[str] = [v for v in text if v in 'aeiouAEIOU' ]
+print(vowels)
+print('Count of vowels:', len(vowels))
+name: list[str] = ['Satyam', 'Harsh', 'Allu Arjun', 'Roman', 'Priyanka']
+print("Length of names: ", [len(names) for names in name])
