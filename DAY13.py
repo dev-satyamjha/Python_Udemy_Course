aef764d2b861c3118f042b844b1148eb247f1c75
dev-satyamjha <satyamjha.me@gmail.com>
2025-11-18 09:55:37 +0530

    Day 13 progress..

--- DAY13.py
+++ DAY13.py
@@ -1,16 +1,24 @@
 # Instance Attributes
 
 class Car:
+    year: int = 2022
+
     def __init__(self, brand: str, name: str, price: int):
         self.brand = brand
         self.name = name
         self.price = price
     def info(self):
-        print(f"{self.brand}'s {self.name} costs ₹{self.price}")
+        print(f"{self.brand}'s {self.name} costs ₹{self.price} that was launched in {self.year}")
 
 Hyundai: Car = Car('Hyundai', 'EV', 1200000)
 TATA: Car = Car('TATA', 'NEXON', 300000)
 Mercedes: Car = Car ('Mercedes', 'Benz', 20000000)
+
+Hyundai.info()
+TATA.info()
+Mercedes.info()
+
+Car.year = 2002
 
 Hyundai.info()
 TATA.info()
