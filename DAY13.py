bb20432059e37bee1be1ce42d3584cbeec2f8cd5
dev-satyamjha <satyamjha.me@gmail.com>
2025-11-19 15:56:13 +0530

    Day 13 progress..

--- DAY13.py
+++ DAY13.py
@@ -1,5 +1,9 @@
 # Instance Attributes
 
+from ast import Num
+from pickletools import float8
+
+
 class Car:
     year: int = 2022
 
@@ -18,8 +22,25 @@ Hyundai.info()
 TATA.info()
 Mercedes.info()
 
+#Class Attributes
+
 Car.year = 2002
 
 Hyundai.info()
 TATA.info()
 Mercedes.info()
+
+#Dunder Methods
+
+class Number:
+    def __init__(self, n: int):
+        self.n = n
+
+    def __add__(self, other: 'Number') -> float:
+        return self.n + other.n
+
+    def __mul__(self, other: 'Number') -> float:
+        return self.n * other.n
+
+print(Number(10) + Number(20))
+print(Number(10) * Number(20))
