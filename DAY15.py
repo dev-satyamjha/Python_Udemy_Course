b992a82b7758ca44187f8a697348329d3ef08a78
dev-satyamjha <satyamjha.me@gmail.com>
2025-11-26 23:59:09 +0530

    Day 15 progress..

--- DAY15.py
+++ DAY15.py
@@ -24,3 +24,31 @@ dog: Animal = Animal('Harry')
 
 dog.eat()
 dog.sleep()
+
+# super()
+
+class Computer:
+    def __init__(self, brand: str, price: int) -> None:
+        self.brand = brand
+        self.price = price
+
+    def power_on(self):
+        print(f"{self.brand} is turning on...")
+    def charge(self):
+        print(f"{self.brand} is charging...")
+    def shut_down(self):
+        print(f"{self.brand} is shutting down...")
+
+class Linux(Computer):
+    def __init__(self, price: int) -> None:
+        super().__init__('Linux', price)
+    def update(self):
+        print(f"Your {self.brand} is updating...")
+    def security(self):
+        print(f"{self.brand} is asking for password...")
+
+linux: Linux = Linux(20000)
+linux.power_on()
+linux.update()
+linux.security()
+linux.shut_down()
