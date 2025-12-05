81c10722d68636f42d76b14600a1d5bd66583c0d
dev-satyamjha <satyamjha.me@gmail.com>
2025-12-05 23:50:39 +0530

    Day 17 starts..

---
+++ DAY17.py
@@ -0,0 +1,15 @@
+#all()
+
+output: list[int] = [1,1,1,0,0,0]
+print(all(output))
+
+string: list[str] = ['Past', '', 'Gone', 'Fight']
+print(all(string))
+
+words: list[str] = ['System', 'Battery', 'Vehicle', 'Wife']
+if all(words):
+    print("The word list is not empty.")
+else:
+    print("The list is non-empty.")
+
+print(all([]))
