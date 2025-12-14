b39d19c76b146a301902a550edac3f6db820517c
dev-satyamjha <satyamjha.me@gmail.com>
2025-12-14 23:14:59 +0530

    Day 19 progress..

--- DAY19.py
+++ DAY19.py
@@ -7,3 +7,23 @@ def square(n):
 print(square.__doc__)
 
 print(print.__doc__)
+
+print()
+
+#Assert
+
+def bank_account() -> None:
+    account: int = 0
+    assert account >=0
+
+    print(f"The account is created with a balance of ₹ {account}")
+
+bank_account()
+
+def grade(grade:int) ->str:
+    assert 0<=grade<=100, f'Invalid {grade}'
+    return 'passed' if grade>=70 else 'failed'
+
+print(grade(45))
+print(grade(100))
+print(grade(75))
